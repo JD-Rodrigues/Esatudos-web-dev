97c7230507c8a0b3f4cabe92302ceaa3049b1305
JD_Rodrigues <jdevproject01@gmail.com>
2022-06-09 14:20:37 -0300

    Código sem comentários

--- Redux/3 - my-todo-react-redux-vanilla/src/App.js
+++ Redux/3 - my-todo-react-redux-vanilla/src/App.js
@@ -8,22 +8,22 @@ import {updateTasks} from './assets/redux/actions/tasksActions'
 import store from './assets/redux/store'
 
 function App() {
-  //const tasks = useSelector(globalState=>globalState.tasksReducer.state)
+  const tasks = useSelector(globalState=>globalState.tasksReducer.state)
   const modalDelete = useSelector(state=>state.modalDeleteReducer)
-  // const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-  // const UPDATED_TASKS = JSON.parse(localStorage.getItem('UPDATED_ITEMS'))  
+  const UPDATED_TASKS = JSON.parse(localStorage.getItem('UPDATED_ITEMS'))  
 
-  // useEffect(()=>{
-  //   if (UPDATED_TASKS)  {
-  //     dispatch(updateTasks(UPDATED_TASKS))
-  //   }
+  useEffect(()=>{
+    if (UPDATED_TASKS)  {
+      dispatch(updateTasks(UPDATED_TASKS))
+    }
     
-  // },[])
+  },[])
 
-  // useEffect(()=>{
-  //   localStorage.setItem('UPDATED_ITEMS',JSON.stringify(tasks))
-  // },[tasks])
+  useEffect(()=>{
+    localStorage.setItem('UPDATED_ITEMS',JSON.stringify(tasks))
+  },[tasks])
  
   return (
     <div className="container">
